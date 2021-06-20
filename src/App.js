7fe78e35dfa9e5de82ec21f2513ba1d4bda64986
marksterben <68014273+marksterben@users.noreply.github.com>
2021-06-20 17:14:51 +0700

    create container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Container from './components/Container';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Container></Container>
     </>
   );
 }
