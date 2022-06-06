8f3166c15a5a6c73e14b7a24028167b3dbee597d
Marcellino <68014273+marksterben@users.noreply.github.com>
2022-06-06 09:23:07 +0700

    Revert "implement module federation"
    This reverts commit 11322a0b7e3c3ae72289e9a57fceaa8cd6237339.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import("./bootstrap");
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import "./index.css";
+import App from "./App";
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
