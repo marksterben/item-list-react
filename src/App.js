26ba97e33451da694560c83f149bb7d62bad2985
Marcellino <68014273+marksterben@users.noreply.github.com>
2022-06-06 09:22:41 +0700

    Revert "..."
    This reverts commit 58fe740d19e443f2ccbfadf539710f2b898907fd.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import { QueryClient, QueryClientProvider } from "react-query";
+import { useQuery } from "react-query";
 
-import "./index.css";
-import Layout from "./Layout";
+import Navbar from "./components/Navbar";
+import Container from "./components/Container";
+import ItemInput from "./components/ItemInput";
+import Items from "./components/Items";
+import Empty from "./components/Empty";
+import Info from "./components/Info";
 
-const queryClient = new QueryClient();
+import { getItems } from "./rest";
 
 const App = () => {
+  const { isLoading, isError, data } = useQuery("items", getItems);
+
+  if (isLoading) {
+    return (
+      <>
+        <Navbar />
+        <Container>
+          <Empty text="Loading..." />
+        </Container>
+      </>
+    );
+  }
+
+  if (isError) {
+    return (
+      <>
+        <Navbar />
+        <Container>
+          <Empty text="There is an error!" />
+        </Container>
+      </>
+    );
+  }
+
+  const items = data.items;
+  const totalCounts = items.reduce((total, num) => total + num.count, 0);
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <Layout />
-    </QueryClientProvider>
+    <>
+      <Navbar />
+      <Container>
+        <ItemInput />
+
+        <Info itemsLength={items.length} totalCounts={totalCounts} />
+
+        {items.length > 0 ? (
+          <Items items={items} />
+        ) : (
+          <Empty text="Empty List ..." />
+        )}
+      </Container>
+    </>
   );
 };
 
